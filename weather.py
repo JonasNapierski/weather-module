d0005b4f454a8d7a60fbaa39f94a365488a2d3f6
Leif <leifjahnel@gmail.com>
2021-06-15 12:46:23 +0200

    implemented keywords

--- weather.py
+++ weather.py
@@ -1,17 +1,28 @@
 import json 
 import requests
 
+def stadt_angegeben(mm):
+    """ if msg contains "in" returns city
+    """
+    for i in range(len(mm)):
+        if mm[i] == "in":
+            city = mm[i+1]
+    return city
+
+def stadt_fehlt(msg, user):
+    """ if: city found, returns lat, lon
+        else: None
+    """
+    cfg = user.get_module_config("weather-module")
+    for keywordobject in cfg["keywords"]:
+        if msg.__contains__(keywordobject["keyword"]):
+            return keywordobject
+    return None
+
 def exec(msg, user):
 
     cfg = user.get_module_config("weather-module")
     mm = msg.split(" ")
-
-    print(msg)
-    print(mm)
-
-    for i in range(len(mm)):
-        if mm[i] == "in":
-            city = mm[i+1]
 
     APIKEY = cfg['api_token']
     language = cfg['language']
@@ -19,12 +30,25 @@ def exec(msg, user):
     degree = {"standart":"°K","imperial":"°F","metric": "°C"}
     grad = degree.get(units)
 
-    api_url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&lang={language}&units={units}&appid={APIKEY}"           
+    print(msg)
+    print(mm)
 
-    data = requests.post(api_url).json()
-    temp = round(float(data['main']['temp']))
-    #return data
-    try:
-        return {"temp": temp, "msg": f"In {city} sind es {temp} {grad}", "user": user.uuid}
-    except:
-        return {"cod": 500, "user": user.uuid}
+    if stadt_fehlt(msg, user) == None:
+        city = stadt_angegeben(mm)
+        city_url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&lang={language}&units={units}&appid={APIKEY}"
+        data = requests.post(city_url).json()
+        if data["cod"] == 200:
+            temp = round(float(data['main']['temp']))
+            return {"temp": temp, "msg": f"In {city} sind es {temp}{grad}", "user": user.uuid}
+        else:
+            return {"cod": 500, "user": user.uuid}
+    else:
+        keywordobject = stadt_fehlt(msg, user)
+        cord_url = f"https://api.openweathermap.org/data/2.5/weather?lat={keywordobject['lat']}&lon={keywordobject['lon']}&lang={language}&units={units}&appid={APIKEY}"
+        data = requests.post(cord_url).json()
+
+        if data["cod"] == 200:
+            temp = round(float(data['main']['temp'])) 
+            return {"temp": temp, "msg": f"{keywordobject['out']} sind es {temp}{grad}", "user": user.uuid}
+        else:
+            return {"cod": 500, "user": user.uuid}
