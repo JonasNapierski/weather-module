c81371b5c688488d8ce6ee4ad2a40b86c68e9e52
Leif <leifjahnel@gmail.com>
2021-06-26 15:01:27 +0200

    new forecast function

--- weather.py
+++ weather.py
@@ -1,6 +1,5 @@
 import json 
 import requests
-import datetime
 
 def stadt_angegeben(mm):
     """ if msg contains "in" returns city
@@ -8,7 +7,7 @@ def stadt_angegeben(mm):
     for i in range(len(mm)):
         if mm[i] == "in":
             city = mm[i+1]
-    return city
+            return city
 
 def stadt_fehlt(msg, user):
     """ if: keyword in cofig found, returns lat, lon
@@ -20,18 +19,20 @@ def stadt_fehlt(msg, user):
             return keywordobject
     return None
 
-def forecast(mm):
-    if mm.__contains__('morgen'):
-        today = datetime.datetime.now()
-        new = today + datetime.timedelta(days=1)
-        forecasttime = int(new.timestamp())
-        return forecasttime
-    elif mm.__contains__('übermorgen'):
-        today = datetime.datetime.now()
-        new = today + datetime.timedelta(days=2)
-        forecasttime = int(new.timestamp())
-        return forecasttime
-    return None
+def forecast(msg):
+    if msg.__contains__('morgen'):
+        time = 'daily'
+        section = 1
+        return time, section
+    elif msg.__contains__('übermorgen'):
+        time = 'daily'
+        section = 2
+        return time, section
+    elif msg.__contains__('in einer Stunde'):
+        time = 'hourly'
+        section = 1
+        return time, section
+    return None, None
 
 def exec(msg, user):
 
@@ -43,6 +44,7 @@ def exec(msg, user):
     units = cfg['units']
     degree = cfg['degree']
     grad = degree.get(units)
+
     print(msg)
     print(mm)
 
@@ -50,15 +52,21 @@ def exec(msg, user):
         city = stadt_angegeben(mm)
         city_url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&lang={language}&units={units}&appid={APIKEY}"
         data = requests.post(city_url).json()
-        time = forecast(mm)
+        print(data)
+        time, section = forecast(msg)
+
         if data["cod"] == 200 and time == None:
             temp = round(float(data['main']['temp']))
             return {"temp": temp, "msg": f"In {city} sind es {temp}°{grad}", "user": user.uuid}
+
         elif data["cod"] == 200 and time != None:
             fcast_url = f"https://api.openweathermap.org/data/2.5/onecall?lat={data['coord']['lat']}&lon={data['coord']['lon']}&units={units}&lang={language}&appid={APIKEY}"
             fcast = requests.post(fcast_url).json()
-            print(fcast)
-            temp = int(fcast['daily'][1]['temp']['day'])
+            if time == 'hourly':
+                temp = int(fcast[time][section]['temp'])
+            else:
+                temp = int(fcast[time][section]['temp']['day'])
+                
             return {"temp": temp, "msg": f"In {city} werden es {temp}°{grad}", "user": user.uuid}
         else:
              return {"cod": 500, "user": user.uuid}
@@ -67,9 +75,20 @@ def exec(msg, user):
         keywordobject = stadt_fehlt(msg, user)
         cord_url = f"https://api.openweathermap.org/data/2.5/weather?lat={keywordobject['lat']}&lon={keywordobject['lon']}&lang={language}&units={units}&appid={APIKEY}"
         data = requests.post(cord_url).json()
+        time, section = forecast(msg)
 
-        if data["cod"] == 200:
+        if data["cod"] == 200 and time == None:
             temp = round(float(data['main']['temp'])) 
             return {"temp": temp, "msg": f"{keywordobject['out']} sind es {temp}°{grad}", "user": user.uuid}
+
+        elif data["cod"] == 200 and time != None:
+            fcast_url = f"https://api.openweathermap.org/data/2.5/onecall?lat={data['coord']['lat']}&lon={data['coord']['lon']}&units={units}&lang={language}&appid={APIKEY}"
+            fcast = requests.post(fcast_url).json()
+            if time == 'hourly':
+                temp = int(fcast[time][section]['temp'])
+            else:
+                temp = int(fcast[time][section]['temp']['day'])
+
+            return {"temp": temp, "msg": f"{keywordobject['out']} werden es {temp}°{grad}", "user": user.uuid}             
         else:
             return {"cod": 500, "user": user.uuid}
