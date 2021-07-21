919ce9b0a2dfc29fefb2eff95401cc5f5f47fb2e
Leif <leifjahnel@gmail.com>
2021-07-21 13:31:57 +0200

    implemented predicted commands

--- weather.py
+++ weather.py
@@ -19,26 +19,15 @@ def stadt_fehlt(msg, user):
             return keywordobject
     return None
 
-def forecast(msg):
-    if msg.__contains__('morgen'):
-        time = 'daily'
-        section = 1
-        return time, section
-    elif msg.__contains__('übermorgen'):
-        time = 'daily'
-        section = 2
-        return time, section
-    elif msg.__contains__('in einer Stunde'):
-        time = 'hourly'
-        section = 1
-        return time, section
-    return None, None
 
-def exec(msg, user):
+def exec(msg, user, predicted_cmd):
+
+
 
     cfg = user.get_module_config("weather-module")
     mm = msg.split(" ")
-
+    command = predicted_cmd.split("-")
+    print(command)
     APIKEY = cfg['api_token']
     language = cfg['language']
     units = cfg['units']
@@ -48,47 +37,36 @@ def exec(msg, user):
     print(msg)
     print(mm)
 
+
     if stadt_fehlt(msg, user) == None:
-        city = stadt_angegeben(mm)
-        city_url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&lang={language}&units={units}&appid={APIKEY}"
-        data = requests.post(city_url).json()
-        #print(data)
-        time, section = forecast(msg)
+            city = stadt_angegeben(mm)
+            if command[0] == 'temp':
+                city_url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&lang={language}&units={units}&appid={APIKEY}"
+                data = requests.post(city_url).json()
+                if data['cod'] == 200:
+                    fcast_url = f"https://api.openweathermap.org/data/2.5/onecall?lat={data['coord']['lat']}&lon={data['coord']['lon']}&units={units}&lang={language}&appid={APIKEY}"
+                    fcast = requests.post(fcast_url).json()
+                    temp = int(fcast[command[1]][int(command[2])]['temp']['day'])
+                    return {"cod": 200, "temp": temp, "msg": f"In {city} werden es {temp}°{grad}", "user": user.uuid}
 
-        if data["cod"] == 200 and time == None:
-            temp = round(float(data['main']['temp']))
-            return {"temp": temp, "msg": f"In {city} sind es {temp}°{grad}", "user": user.uuid}
-
-        elif data["cod"] == 200 and time != None:
-            fcast_url = f"https://api.openweathermap.org/data/2.5/onecall?lat={data['coord']['lat']}&lon={data['coord']['lon']}&units={units}&lang={language}&appid={APIKEY}"
-            fcast = requests.post(fcast_url).json()
-            if time == 'hourly':
-                temp = int(fcast[time][section]['temp'])
+            elif command[0] == "description":
+                city_url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&lang={language}&units={units}&appid={APIKEY}"
+                data = requests.post(city_url).json()
+                if data['cod'] == 200:
+                    fcast_url = f"https://api.openweathermap.org/data/2.5/onecall?lat={data['coord']['lat']}&lon={data['coord']['lon']}&units={units}&lang={language}&appid={APIKEY}"
+                    fcast = requests.post(fcast_url).json()
+                    description = fcast[command[1]][int(command[2])]['weather'][0]['description']
+                    return {"cod": 200, "msg": f"Es wird {description}", "user": user.uuid}
             else:
-                temp = int(fcast[time][section]['temp']['day'])
-                
-            return {"temp": temp, "msg": f"In {city} werden es {temp}°{grad}", "user": user.uuid}
-        else:
-             return {"cod": 500, "user": user.uuid}
+                    return {"cod": 500, "msg": "Es ist ein Fehler aufgetreten.", "user": user.uuid}
 
     else:
-        keywordobject = stadt_fehlt(msg, user)
-        cord_url = f"https://api.openweathermap.org/data/2.5/weather?lat={keywordobject['lat']}&lon={keywordobject['lon']}&lang={language}&units={units}&appid={APIKEY}"
-        data = requests.post(cord_url).json()
-        time, section = forecast(msg)
+        if command[0] == 'temp':
+            keywordobject = stadt_fehlt(msg, user)
+            fcast_url = f"https://api.openweathermap.org/data/2.5/onecall?lat={keywordobject['lat']}&lon={keywordobject['lon']}&lang={language}&units={units}&appid={APIKEY}"
+            fcast = requests.post(cord_url).json()
+            temp = int(fcast[command[1]][int(command[2])]['temp']['day'])
 
-        if data["cod"] == 200 and time == None:
-            temp = round(float(data['main']['temp'])) 
-            return {"temp": temp, "msg": f"{keywordobject['out']} sind es {temp}°{grad}", "user": user.uuid}
-
-        elif data["cod"] == 200 and time != None:
-            fcast_url = f"https://api.openweathermap.org/data/2.5/onecall?lat={data['coord']['lat']}&lon={data['coord']['lon']}&units={units}&lang={language}&appid={APIKEY}"
-            fcast = requests.post(fcast_url).json()
-            if time == 'hourly':
-                temp = int(fcast[time][section]['temp'])
-            else:
-                temp = int(fcast[time][section]['temp']['day'])
-
-            return {"temp": temp, "msg": f"{keywordobject['out']} werden es {temp}°{grad}", "user": user.uuid}             
+            return {"cod": 200, "temp": temp, "msg": f"{keywordobject['out']} werden es {temp}°{grad}", "user": user.uuid}             
         else:
-            return {"cod": 500, "user": user.uuid}
+            return {"cod": 500, "msg": "Es ist ein Fehler aufgetreten.", "user": user.uuid}
