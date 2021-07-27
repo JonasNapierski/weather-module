bd02e3c3eb239def9328f07e085ef46b2d1cfe52
Leif <leifjahnel@gmail.com>
2021-07-27 13:11:17 +0200

    fixed issue #2

--- weather.py
+++ weather.py
@@ -25,6 +25,7 @@ def command_city(city,language,units,APIKEY):
     if data['cod'] == 200:
         fcast_url = f"https://api.openweathermap.org/data/2.5/onecall?lat={data['coord']['lat']}&lon={data['coord']['lon']}&units={units}&lang={language}&appid={APIKEY}"
         fcast = requests.post(fcast_url).json()
+        print(fcast)
         return fcast
 
 def command_cord(language,units,APIKEY,msg,user):
@@ -43,33 +44,36 @@ def exec(msg, user, predicted_cmd):
     units = cfg['units']
     degree = cfg['degree']
     grad = degree.get(units)
+    status = requests.post(f"http://api.openweathermap.org/data/2.5/weather?q=Hamburg&appid={APIKEY}").json()
 
-    print(msg)
-    print(mm)
-    print(command)
+    if status['cod'] == 200:
+        if city_name_missing(msg, user) == None:
+            city = city_name_given(mm)
+            if command[0] == 'temp':
+                fcast = command_city(city,language,units,APIKEY)
+                temp = int(fcast[command[1]][int(command[2])]['temp']['day'])
+                return {"cod": 200, "temp": temp, "msg": f"In {city} werden es {temp}°{grad}", "user": user.uuid}
 
-    if city_name_missing(msg, user) == None:
-        city = city_name_given(mm)
-        if command[0] == 'temp':
-            fcast = command_city(city,language,units,APIKEY)
-            temp = int(fcast[command[1]][int(command[2])]['temp']['day'])
-            return {"cod": 200, "temp": temp, "msg": f"In {city} werden es {temp}°{grad}", "user": user.uuid}
+            elif command[0] == "description":
+                fcast = command_city(city,language,units,APIKEY)
+                description = fcast[command[1]][int(command[2])]['weather'][0]['description']
+                return {"cod": 200, "msg": f"Es wird {description}", "user": user.uuid}
+            else:
+                return {"cod": 500, "msg": "Es ist ein Fehler aufgetreten.", "user": user.uuid}
 
-        elif command[0] == "description":
-            fcast = command_city(city,language,units,APIKEY)
-            description = fcast[command[1]][int(command[2])]['weather'][0]['description']
-            return {"cod": 200, "msg": f"Es wird {description}", "user": user.uuid}
         else:
-            return {"cod": 500, "msg": "Es ist ein Fehler aufgetreten.", "user": user.uuid}
+            if command[0] == 'temp':
+                fcast = command_cord(language, units, APIKEY, msg, user)
+                temp = int(fcast[command[1]][int(command[2])]['temp']['day'])
+                return {"cod": 200, "temp": temp, "msg": f"{keywordobject['out']} werden es {temp}°{grad}", "user": user.uuid}
+            elif command[0] == "description":
+                fcast = command_cord(language, units, APIKEY, msg, user)
+                description = fcast[command[1]][int(command[2])]['weather'][0]['description']
+                return {"cod": 200, "msg": f"Es wird {description}", "user": user.uuid}          
+            else:
+                return {"cod": 500, "msg": "Es ist ein Fehler aufgetreten.", "user": user.uuid}
 
+    elif status['cod'] == 401:
+        return {"cod": 401, "msg": "Invalid Api key. Please check if you inserted an Api key or copied your Api key correctly", "user": user.uuid}
     else:
-        if command[0] == 'temp':
-            fcast = command_cord(language, units, APIKEY, msg, user)
-            temp = int(fcast[command[1]][int(command[2])]['temp']['day'])
-            return {"cod": 200, "temp": temp, "msg": f"{keywordobject['out']} werden es {temp}°{grad}", "user": user.uuid}
-        elif command[0] == "description":
-            fcast = command_cord(language, units, APIKEY, msg, user)
-            description = fcast[command[1]][int(command[2])]['weather'][0]['description']
-            return {"cod": 200, "msg": f"Es wird {description}", "user": user.uuid}          
-        else:
-            return {"cod": 500, "msg": "Es ist ein Fehler aufgetreten.", "user": user.uuid}
+        return {"cod": status['cod'], "msg": "An error ocurred"}
